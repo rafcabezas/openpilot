8abbf11f4a28ec161b36ceac92d78fab96ce868f
raf <rafcabezas@gmail.com>
2019-06-29 13:52:45 -0400

    test fake 0x00e

--- panda/board/safety/safety_tesla.h
+++ panda/board/safety/safety_tesla.h
@@ -1663,6 +1663,7 @@ static void tesla_fwd_to_radar_modded(int bus_num, CAN_FIFOMailBox_TypeDef *to_f
   }
   if (addr == 0x00E )
   {
+    /* 
     to_send.RIR = (0x199 << 21) + (addr_mask & (to_fwd->RIR | 1));
     //force StW_AnglHP_Sens_Id to DELPHI (0x04 1st octet of RDHR)
     to_send.RDHR = to_send.RDHR & 0x00FFFFF0;
@@ -1671,7 +1672,18 @@ static void tesla_fwd_to_radar_modded(int bus_num, CAN_FIFOMailBox_TypeDef *to_f
     to_send.RDHR = to_send.RDHR | (crc << 24);
     can_send(&to_send, bus_num);
     return;
+    */
+    static int tesla_radar_x199_id = 0;
+    to_send.RIR = (0x199 << 21) + (addr_mask & (to_fwd->RIR | 1));
+    to_send.RDLR = 0x00207D2F;
+    to_send.RDHR = 0x0000FF04 + (tesla_radar_x199_id << 20);
+    int crc = add_tesla_crc(to_send.RDLR, to_send.RDHR,7);
+    to_send.RDHR = to_send.RDHR | (crc << 24);
+    tesla_radar_x199_id++;
+    tesla_radar_x199_id = tesla_radar_x199_id % 16;
+    send_fake_message(to_send.RIR, to_send.RDTR, 8, 0x199, tesla_radar_can, to_send.RDLR, to_send.RDHR);
   }
+
   if (addr == 0x20A )
   {
     to_send.RIR = (0x159 << 21) + (addr_mask & (to_fwd->RIR | 1));
@@ -1804,7 +1816,7 @@ static int tesla_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd)
     }
 
     //check all messages we need to also send to radar, moddified, all the time
-    if  (((addr == 0xE ) || (addr == 0x308 ) || (addr == 0x45 ) || (addr == 0x398 ) ||
+    if  ((/*(addr == 0x00E ) || */(addr == 0x308 ) || (addr == 0x45 ) || (addr == 0x398 ) ||
     (addr == 0x405 ) ||  (addr == 0x30A)) && (enable_radar_emulation == 1))  {
       tesla_fwd_to_radar_modded(tesla_radar_can, to_fwd);
     }
